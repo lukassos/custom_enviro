18a52bec0e574c1f4bc525deec27df525520c07b
Lukas Puchon <lukassos@gmail.com>
2016-12-14 22:57:26 +0100

    mod / fix : install.sh  alpha test v00.009

--- raspbian/install.sh
+++ raspbian/install.sh
@@ -5,12 +5,12 @@ remove_old_lines()
 	line_end=`cat ~/.bashrc | grep CUSTOM -n | cut -d ":" -f1 | tail -1`
 	line_head="`echo "$line_start - 1" | bc`"
 	line_count="`wc -l  ~/.bashrc | cut -f1 -d " "`"
-	line_tail=`echo "$line_count - $line_end + 1" | bc`
+	line_tail=`echo "$line_count - $line_end " | bc`
 	
 	cp ~/.bashrc ~/.bashrc.bak
 	head ~/.bashrc -n "$line_head" > ~/.bashrc.new
 	tail ~/.bashrc -n "$line_tail" >> ~/.bashrc.new
-	#mv ~/.bashrc.new ~/.bashrc
+	mv ~/.bashrc.new ~/.bashrc
 }
 
 check_version()
@@ -18,23 +18,30 @@ check_version()
 	if [ -n "$1"  ]; then 
 	if [ -n "$2"  ]; then 
 		if [ "$1" != "$2" ]; then 
+			echo "Removing old aliases"
 			remove_old_lines
 		fi
 	fi
 	fi
+	echo "Adding new aliases"
+	cat ../install_current/bashrc >> ~/.bashrc
+
 }
 
 
-current_version="dgsfasdfsdf"
+current_version=`cat ../install_current/bashrc | grep "CUSTOM ALIASES"`
 already_in=`cat ~/.bashrc | grep "CUSTOM ALIASES"`
+echo "------------"
+echo "Current version"
+echo $current_version
+echo "------------"
+echo "New version"
+echo $already_in
+echo "------------"
+
 
 check_version $already_in $current_version
 
-
-if [ -z "$already_in" ]; then
-	echo "Adding new aliases"
-	cat ../install_current/bashrc >> ~/.bashrc
-fi
 
 sudo apt-get install colordiff bc
 
